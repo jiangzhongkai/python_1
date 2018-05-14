d4b3c87a33b42507f0e8d7cb0c4091761c08d773
Peter_Jiang <865268033@qq.com>
2018-05-14 13:03:36 +0800

    SGA

--- SGA_Method.py
+++ SGA_Method.py
@@ -6,6 +6,8 @@
 """
 import os
 import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SGA(object):
     """
@@ -24,10 +26,44 @@ class SGA(object):
         self.pop_ac=pop_ac
         pass
 
-    def initial_pop(self,):
+    def initial_pop(self):
+        """
+        初始化种群
+        :return:
+        """
+        pass
+
+    def select_pop(self):
+        """
+        选择相应的种群来进行交叉
+        :return:
+        """
+        pass
+
+    def roll_select(self):
+        """
+        使用轮盘赌的原则来选择交叉的染色体
+        :return:
+        """
         pass
 
     def cross(self):
+        """
+        进行交叉
+        :return:
+        """
         pass
+
     def mute(self):
+        """
+        进行变异
+        :return:
+        """
         pass
+
+
+
+if __name__=="__main__":
+    lstm_sga=SGA(cross_p=0.2,mute_p=0.01,population_size=100000,pop_ac=le-8)
+    lstm_sga.initial_pop()
+    pass

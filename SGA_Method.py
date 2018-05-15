ba895b8f1adf2bd3c5165d78c9d9a4cb5dcf0e19
Peter_Jiang <865268033@qq.com>
2018-05-15 09:09:26 +0800

    SGA

--- SGA_Method.py
+++ SGA_Method.py
@@ -13,11 +13,38 @@ import random
 
 """
 思想:
-1.种群大小->染色体->基因变化
+1.种群大小->染色体->基因
 2.写一个生成染色体类
 3.写一个遗传算法类,
 4.将染色体类生成的多个染色体放到种群中去
+5.利用遗传算法寻找最优解能否减少寻解的时间？
+6.通过遗传算法获得结果传给神经网络是不是会更加快呢？
+
+choose an intial population
+determine the fitness of each individual
+perform selection
+repeat
+    perform crossover
+    perform mutation
+    determine the fitness of each individual
+    perform selection
+until some stopping criterion applies
+
+====
+将遗传算法运用神经网络的难点就是染色体该如何编码？以及评价函数该如何选择？
+   =====辅助结合方式：用GA对数据进行预处理，然后用NN解决问题，例如模式识别用GA进行特征提取，再用nn进行分类
+   =====合作，GA和NN共同处理问题，在NN固定的网络拓扑下，利用GA确定链接权重，或者直接利用GA优选网络结构再用，bp训练网络
 """
+'''
+交叉,变异，适应度，种群，染色体，基因，选择
+
+参数编码
+初始群体的设计
+适应度函数的设计
+遗传操作设计(选择,交叉,变异)
+控制参数的设定
+
+'''
 
 
 class SGA(object):
@@ -47,8 +74,14 @@ class SGA(object):
 
     def initial_pop(self):
         """
+        初始化种群
         :return:
         """
+        for i in range(self.max_generate):
+            #生成一个染色体，然后将染色体放到列表中去
+            rand_x=random.rand(0,1)
+            self.pop.append("染色体")
+
 
         pass
 
@@ -89,6 +122,7 @@ class SGA(object):
         :return:
         """
 
+
         pass
 
 class Choromones(object):
@@ -100,9 +134,6 @@ class Choromones(object):
 
         """
         pass
-
-
-
 
 if __name__=="__main__":
     lstm_sga=SGA(cross_p=0.2,mute_p=0.01,population_size=100000,pop_ac=1e-8)
